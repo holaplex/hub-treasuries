94f03e007d22f7b3e888fa2a81be86a75680b96d
Kyle Espinola <kyle.s.espinola@gmail.com>
2023-06-12 10:09:48 +0200

    refactor: have fireblocks sign abritrary list of requested signatures looking up the vault id based on the provided public key

--- api/src/events/processor.rs
+++ api/src/events/processor.rs
@@ -1,6 +1,5 @@
 use fireblocks::Fireblocks;
-use hub_core::{prelude::*, producer::Producer, uuid::Uuid};
-use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
+use hub_core::{prelude::*, producer::Producer};
 
 use super::{
     customer::CustomerEventHandler, organization::OrganizationEventHandler, polygon::Polygon,
@@ -8,7 +7,6 @@ use super::{
 };
 use crate::{
     db::Connection,
-    entities::{project_treasuries, treasuries},
     proto::{
         customer_events::Event as CustomerEvent, organization_events::Event as OrganizationEvent,
         polygon_nft_events::Event as PolygonNftEvent, solana_nft_events::Event as SolanaNftEvent,
@@ -56,12 +54,11 @@ impl Processor {
             },
             Services::Polygon(key, e) => {
                 let polygon = self.polygon();
-                let signer = polygon.signer();
 
                 #[allow(clippy::single_match)]
                 match e.event {
                     Some(PolygonNftEvent::SubmitCreateDropTxn(payload)) => {
-                        signer.create_drop(key.clone(), payload).await?;
+                        polygon.create_drop(key.clone(), payload).await?;
                     },
 
                     None => (),
@@ -69,30 +66,26 @@ impl Processor {
                 Ok(())
             },
             Services::Solana(key, e) => {
-                let conn = self.db.get();
-                let vault_id =
-                    Self::find_vault_id_by_project_id(conn, key.project_id.clone()).await?;
                 let solana = self.solana();
-                let signer = solana.signer(vault_id);
 
                 match e.event {
-                    Some(SolanaNftEvent::SignCreateDrop(payload)) => {
-                        signer.create_drop(key.clone(), payload).await?;
+                    Some(SolanaNftEvent::CreateDropSigningRequested(payload)) => {
+                        solana.create_drop(key.clone(), payload).await?;
                     },
-                    Some(SolanaNftEvent::SignUpdateDrop(payload)) => {
-                        signer.update_drop(key.clone(), payload).await?;
+                    Some(SolanaNftEvent::UpdateDropSigningRequested(payload)) => {
+                        solana.update_drop(key.clone(), payload).await?;
                     },
-                    Some(SolanaNftEvent::SignMintDrop(payload)) => {
-                        signer.mint_drop(key.clone(), payload).await?;
+                    Some(SolanaNftEvent::MintDropSigningRequested(payload)) => {
+                        solana.mint_drop(key.clone(), payload).await?;
                     },
-                    Some(SolanaNftEvent::SignTransferAsset(payload)) => {
-                        signer.transfer_asset(key.clone(), payload).await?;
+                    Some(SolanaNftEvent::TransferAssetSigningRequested(payload)) => {
+                        solana.transfer_asset(key.clone(), payload).await?;
                     },
-                    Some(SolanaNftEvent::SignRetryCreateDrop(payload)) => {
-                        signer.retry_mint_drop(key.clone(), payload).await?;
+                    Some(SolanaNftEvent::RetryCreateDropSigningRequested(payload)) => {
+                        solana.retry_mint_drop(key.clone(), payload).await?;
                     },
-                    Some(SolanaNftEvent::SignRetryMintDrop(payload)) => {
-                        signer.retry_mint_drop(key.clone(), payload).await?;
+                    Some(SolanaNftEvent::RetryMintDropSigningRequested(payload)) => {
+                        solana.retry_mint_drop(key.clone(), payload).await?;
                     },
                     _ => (),
                 };
@@ -111,28 +104,14 @@ impl Processor {
     }
 
     fn solana(&self) -> Solana {
-        Solana::new(self.fireblocks.clone(), self.producer.clone())
+        Solana::new(
+            self.fireblocks.clone(),
+            self.producer.clone(),
+            self.db.clone(),
+        )
     }
 
     fn polygon(&self) -> Polygon {
         Polygon::new(self.fireblocks.clone(), self.producer.clone())
-    }
-
-    async fn find_vault_id_by_project_id(
-        db: &DatabaseConnection,
-        project: String,
-    ) -> Result<String> {
-        let project = Uuid::from_str(&project)?;
-
-        let (_, t) = project_treasuries::Entity::find()
-            .find_also_related(treasuries::Entity)
-            .filter(project_treasuries::Column::ProjectId.eq(project))
-            .one(db)
-            .await?
-            .context("treasury not found in database")?;
-
-        let t = t.ok_or(anyhow!("treasury not found"))?;
-
-        Ok(t.vault_id)
     }
 }
--- api/src/events/solana.rs
+++ api/src/events/solana.rs
@@ -1,46 +1,41 @@
 use fireblocks::Fireblocks;
+use futures::future::try_join_all;
 use hex::FromHex;
-use hub_core::{prelude::*, producer::Producer};
+use hub_core::{anyhow::Error, prelude::*, producer::Producer};
 
-use super::signer::{Events, Sign, TransactionSigner, Transactions};
+use super::signer::{Events, Sign, Transactions};
 use crate::{
+    db::Connection,
     entities::sea_orm_active_enums::TxType,
     proto::{
         treasury_events::{Event, SolanaSignedTxn},
-        SolanaNftEventKey, SolanaTransaction, TreasuryEventKey, TreasuryEvents,
+        SolanaNftEventKey, SolanaPendingTransaction, TreasuryEventKey, TreasuryEvents,
     },
 };
 
 pub struct Solana {
     fireblocks: Fireblocks,
     producer: Producer<TreasuryEvents>,
+    db: Connection,
 }
 
 impl Solana {
     #[must_use]
-    pub fn new(fireblocks: Fireblocks, producer: Producer<TreasuryEvents>) -> Self {
+    pub fn new(fireblocks: Fireblocks, producer: Producer<TreasuryEvents>, db: Connection) -> Self {
         Self {
             fireblocks,
             producer,
+            db,
         }
-    }
-
-    #[must_use]
-    pub fn signer(&self, vault_id: String) -> TransactionSigner {
-        TransactionSigner::new(
-            self.fireblocks.clone(),
-            self.producer.clone(),
-            Some(vault_id),
-        )
     }
 }
 
 #[async_trait]
-impl Transactions<SolanaNftEventKey, SolanaTransaction, SolanaSignedTxn> for TransactionSigner {
+impl Transactions<SolanaNftEventKey, SolanaPendingTransaction, SolanaSignedTxn> for Solana {
     async fn create_drop(
         &self,
         key: SolanaNftEventKey,
-        payload: SolanaTransaction,
+        payload: SolanaPendingTransaction,
     ) -> Result<SolanaSignedTxn> {
         let tx = self
             .send_transaction(TxType::CreateDrop, key.clone(), payload)
@@ -54,7 +49,7 @@ impl Transactions<SolanaNftEventKey, SolanaTransaction, SolanaSignedTxn> for Tra
     async fn update_drop(
         &self,
         key: SolanaNftEventKey,
-        payload: SolanaTransaction,
+        payload: SolanaPendingTransaction,
     ) -> Result<SolanaSignedTxn> {
         let tx = self
             .send_transaction(TxType::UpdateMetadata, key.clone(), payload)
@@ -68,7 +63,7 @@ impl Transactions<SolanaNftEventKey, SolanaTransaction, SolanaSignedTxn> for Tra
     async fn mint_drop(
         &self,
         key: SolanaNftEventKey,
-        payload: SolanaTransaction,
+        payload: SolanaPendingTransaction,
     ) -> Result<SolanaSignedTxn> {
         let tx = self
             .send_transaction(TxType::MintEdition, key.clone(), payload)
@@ -81,7 +76,7 @@ impl Transactions<SolanaNftEventKey, SolanaTransaction, SolanaSignedTxn> for Tra
     async fn transfer_asset(
         &self,
         key: SolanaNftEventKey,
-        payload: SolanaTransaction,
+        payload: SolanaPendingTransaction,
     ) -> Result<SolanaSignedTxn> {
         let tx = self
             .send_transaction(TxType::TransferMint, key.clone(), payload)
@@ -94,7 +89,7 @@ impl Transactions<SolanaNftEventKey, SolanaTransaction, SolanaSignedTxn> for Tra
     async fn retry_create_drop(
         &self,
         key: SolanaNftEventKey,
-        payload: SolanaTransaction,
+        payload: SolanaPendingTransaction,
     ) -> Result<SolanaSignedTxn> {
         let tx = self
             .send_transaction(TxType::CreateDrop, key.clone(), payload)
@@ -107,7 +102,7 @@ impl Transactions<SolanaNftEventKey, SolanaTransaction, SolanaSignedTxn> for Tra
     async fn retry_mint_drop(
         &self,
         key: SolanaNftEventKey,
-        payload: SolanaTransaction,
+        payload: SolanaPendingTransaction,
     ) -> Result<SolanaSignedTxn> {
         let tx = self
             .send_transaction(TxType::MintEdition, key.clone(), payload)
@@ -119,47 +114,73 @@ impl Transactions<SolanaNftEventKey, SolanaTransaction, SolanaSignedTxn> for Tra
 }
 
 #[async_trait]
-impl Sign<SolanaNftEventKey, SolanaTransaction, SolanaSignedTxn> for TransactionSigner {
+impl Sign<SolanaNftEventKey, SolanaPendingTransaction, SolanaSignedTxn> for Solana {
     async fn send_transaction(
         &self,
         tx_type: TxType,
         key: SolanaNftEventKey,
-        mut payload: SolanaTransaction,
+        mut payload: SolanaPendingTransaction,
     ) -> Result<SolanaSignedTxn> {
+        let conn = self.db.get();
         let note = format!(
             "{:?} by {:?} for project {:?}",
-            tx_type, key.user_id, key.project_id
+            tx_type, key.user_id, key.project_id,
         );
 
-        let vault_id = self.vault_id.clone().context("vault id not set")?;
+        info!("looking up vault ids for {:?}", payload.request_signatures.clone());
+        let vault_ids =
+            Self::find_vault_ids_by_wallet_address(conn, payload.request_signatures.clone()).await?;
+
+        info!("vault ids {:?}", vault_ids.clone());
         let asset_id = self.fireblocks.assets().id("SOL");
+        let create_client = self.fireblocks.client().create();
 
-        let transaction = self
-            .fireblocks
-            .client()
-            .create()
-            .raw_transaction(asset_id, vault_id, payload.serialized_message.clone(), note)
-            .await?;
+        let transactions = vault_ids.into_iter().map(|vault_id| {
+            let asset_id = asset_id.clone();
+            let note = note.clone();
+            let message = payload.serialized_message.clone();
 
-        let transaction_details = self
-            .fireblocks
-            .client()
-            .wait_on_transaction_completion(transaction.id)
-            .await?;
+            info!("sending transaction to fireblocks {vault_id}");
+            create_client.raw_transaction(asset_id, vault_id, message, note)
+        });
+        let transactions = try_join_all(transactions).await?;
 
-        let full_sig = transaction_details
-            .signed_messages
-            .get(0)
-            .context("failed to get signed message response")?
-            .clone()
-            .signature
-            .full_sig;
+        let transaction_details = transactions.into_iter().map(|transaction| {
+            info!("waiting on transaction {:?}", transaction);
 
-        let signature = <[u8; 64]>::from_hex(full_sig)?;
+            self.fireblocks
+                .client()
+                .wait_on_transaction_completion(transaction.id)
+        });
 
-        payload
-            .signed_message_signatures
-            .push(bs58::encode(signature).into_string());
+        let transaction_details = try_join_all(transaction_details).await?;
+
+        let signatures = transaction_details
+            .iter()
+            .map(|transaction_detail| {
+                info!("getting signature for transaction {:?}", transaction_detail);
+
+                let full_sig = transaction_detail
+                    .signed_messages
+                    .get(0)
+                    .context("failed to get signed message response")?
+                    .clone()
+                    .signature
+                    .full_sig;
+
+                let signature = <[u8; 64]>::from_hex(full_sig)?;
+
+                let signature = bs58::encode(signature).into_string();
+
+                Ok(signature)
+            })
+            .collect::<Result<Vec<String>, Error>>()?;
+
+        for signature in signatures {
+            payload.signed_message_signatures.push(signature);
+        }
+
+        info!("transaction signed successfully {:?}", payload);
 
         Ok(SolanaSignedTxn {
             serialized_message: payload.serialized_message,
@@ -169,7 +190,7 @@ impl Sign<SolanaNftEventKey, SolanaTransaction, SolanaSignedTxn> for Transaction
 }
 
 #[async_trait]
-impl Events<SolanaNftEventKey, SolanaSignedTxn> for TransactionSigner {
+impl Events<SolanaNftEventKey, SolanaSignedTxn> for Solana {
     async fn on_create_drop(&self, key: SolanaNftEventKey, tx: SolanaSignedTxn) -> Result<()> {
         let event = TreasuryEvents {
             event: Some(Event::SolanaCreateDropSigned(SolanaSignedTxn {
