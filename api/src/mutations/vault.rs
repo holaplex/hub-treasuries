af2c9a50a4c9efb49868e61137325f95dbbb7711
imabdulbasit <baronbasit@gmail.com>
2023-08-24 01:59:35 +0500

    Validate wallet address

--- api/src/mutations/vault.rs
+++ api/src/mutations/vault.rs
@@ -2,7 +2,7 @@ use async_graphql::{Context, Error, InputObject, Object, Result, SimpleObject};
 use fireblocks::{objects::vault::CreateVaultWallet, Fireblocks};
 use hub_core::{
     chrono::Utc,
-    credits::{CreditsClient, DeductionErrorKind, TransactionId},
+    credits::{CreditsClient, TransactionId},
     producer::Producer,
 };
 use sea_orm::{prelude::*, JoinType, QuerySelect, Set};
--- api/src/queries/wallet.rs
+++ api/src/queries/wallet.rs
@@ -1,7 +1,7 @@
-use async_graphql::{Context, Object, Result};
+use async_graphql::{Context, Error, Object, Result};
+use hub_core::util::ValidateAddress;
 
 use crate::{entities::wallets::Model, AppContext};
-
 #[derive(Debug, Clone, Copy, Default)]
 pub struct Query;
 
@@ -17,6 +17,10 @@ impl Query {
         ctx: &Context<'_>,
         #[graphql(key)] address: String,
     ) -> Result<Option<Model>> {
+        if !ValidateAddress::is_blockchain_address(&address) {
+            return Err(Error::new("Invalid address"));
+        }
+
         let AppContext { wallet_loader, .. } = ctx.data::<AppContext>()?;
 
         wallet_loader.load_one(address).await
